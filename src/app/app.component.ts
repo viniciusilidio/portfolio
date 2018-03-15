ebdb8751ebeae60eea91f5aaf91b8bd8a3e40188
Vinícius Ilídio <viniciusilidio@gmail.com>
2018-03-15 15:02:21 -0300

    varias coisas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'teste';
+
+  toggleSidebar () {
+    $('#sidebar').toggleClass('active');
+    $('#sidebarCollapse').toggleClass('active');
+  }
 }
